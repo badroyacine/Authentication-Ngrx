f1eebf7f79903a1f90ab63aff6fed28b4c60f472
badrooo <badreddine.yacine99@gmail.com>
2020-02-12 13:10:22 +0000

    main structure

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: 'login', component:  LoginComponent, canActivate: [VisitorGuard] },
+  // { path: 'register', component:  RegisterComponent, canActivate: [VisitorGuard] },
+  // { path: 'forgot-password', component:  ForgotPasswordComponent, canActivate: [VisitorGuard] },
+  // { path: '', component:  HomeComponent, canActivate: [AuthGuard] }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ], exports: [
+    RouterModule,
+  ],
+  providers: [],
+  declarations: []
+})
+
+export class AuthRoutingModule { }
