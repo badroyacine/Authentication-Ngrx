b9fe5afa056d1e63505c8e758e059affb251136b
badrooo <badreddine.yacine99@gmail.com>
2020-02-13 08:43:13 +0000

    login and auth service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  baseUrl = environment.baseUrl;
+  
+  constructor(private http: HttpClient) { }
+
+  login(credentials): Observable<any> {
+    console.log(`${this.baseUrl}/users/login`, credentials);
+    return this.http.post<any>(`${this.baseUrl}/users/login`, credentials);
+  }
+
+  signup(credentials): Observable<any> {
+    console.log(`${this.baseUrl}/users/signup`, credentials);
+    return this.http.post<any>(`${this.baseUrl}/users/signup`, credentials);
+  }
+
+  logout(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/users/logout`);
+  }
 }
